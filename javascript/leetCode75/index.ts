ee0f62e6f2e36b821609fe038fc2a6307e3cb583
selim-kerimow <selimkerimow68@gmail.com>
2025-02-23 23:03:31 +0500

    solved problem => 23.02.2025

--- javascript/leetCode75/index.ts
+++ javascript/leetCode75/index.ts
@@ -9,4 +9,13 @@ function mergeAlternately(word1: string, word2: string): string {
   return result;
 };
 
-console.log(mergeAlternately('ac', 'pqr'))
+
+
+
+// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/description/?envType=study-plan-v2&envId=leetcode-75
+function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
+  const max = Math.max(...candies)
+  return candies.map(i => i + extraCandies >= max)
+};
+
+console.log(kidsWithCandies([2,3,5,1,3], 3))
