82ac6993d3e48dbe4a06a4b978ffd85afe63e5dc
selim-kerimow <selimkerimow68@gmail.com>
2025-03-16 01:20:09 +0500

    solved problem => 15.03.2025

--- javascript/leetCode75/index.ts
+++ javascript/leetCode75/index.ts
@@ -18,4 +18,50 @@ function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
   return candies.map(i => i + extraCandies >= max)
 };
 
-console.log(kidsWithCandies([2,3,5,1,3], 3))
+
+
+
+// https://leetcode.com/problems/can-place-flowers/?envType=study-plan-v2&envId=leetcode-75
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  const zeroLength = flowerbed.filter(i => i == 0).length;
+  const oneLength = flowerbed.filter(i => i == 1).length;
+
+  if (n == 1 && zeroLength - oneLength > 1) return true;
+  else return zeroLength > n + 2;
+};
+
+
+
+
+
+// https://leetcode.com/problems/move-zeroes/?envType=study-plan-v2&envId=leetcode-75
+function moveZeroes(nums: number[]): void {
+  let index = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[index] == 0) {
+      nums.splice(index, 1);
+      nums.push(0)
+    }
+    else index++;
+  }
+};
+
+
+
+
+
+// https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=leetcode-75
+function isSubsequence(s: string, t: string): boolean {
+  let indexS = 0;
+  let indexT = 0;
+
+  while (indexS < s.length && indexT < t.length) {
+    if (s[indexS] === t[indexT]) indexS++;
+    indexT++;
+  }
+
+  return indexS === s.length;
+};
+
+console.log(isSubsequence('ace', "abcde"))
